9a535fa37aac2217c2bfc0b4b9704439a7df7384
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-08-08 11:22:39 +0530

    DecisionTree.py

---
+++ Practical 7/Practical 7 Task/DecisionTree.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+data = pd.read_csv("netflix_titles.csv")
+data['type'] = le.fit_transform(data['type'])
+X = data[['release_year']]
+y = data['type']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+print("Training Accuracy : ",model.score(X_train, y_train)*100)
+print("Testing Accuracy : ",model.score(X_test, y_test)*100)
+prediction = model.predict([X_test.iloc[0]])
+print("Predicted Class : ",prediction[0])
+print("Saqlain Khan T013")
