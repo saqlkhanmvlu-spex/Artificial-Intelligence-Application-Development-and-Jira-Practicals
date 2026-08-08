210528cbf6b5c572c0c9d9ada6bf8cbff67837c9
saqlkhanmvlu-spex <saqlkhanmvlu@gmail.com>
2026-08-08 11:19:42 +0530

    Prac7(8).py

---
+++ Practical 7/Practical 7 (6,7 & 8)/Prac7(8).py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+data = pd.read_csv("iris.csv")
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+print("Training Accuracy : ",model.score(X_train, y_train)*100)
+print("Testing Accuracy : ",model.score(X_test, y_test)*100)
+prediction = model.predict([X_test[0]])
+print("Predicted Class : ",prediction[0])
+print("Saqlain Khan T013")
